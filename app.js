ab83428186931f1440a2a5b752a2616b1b9c8ab8
JarekSopinski <jarek.sopinski@gmail.com>
2018-06-25 11:47:06 +0200

    old way / new way

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const events = require('events');
 const util = require('util');
 
-const Person = function(name){this.name = name};
+// const Person = function(name){this.name = name};
 
-util.inherits(Person, events.EventEmitter);
+// util.inherits(Person, events.EventEmitter);
+
+class Person extends events.EventEmitter{
+    constructor(name){
+        super();
+        this.name = name
+    }
+};
 
 const james = new Person('james');
 const mary = new Person('mary');
