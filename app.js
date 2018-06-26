e9b4b5234cfecdd9769f5ee5ad9cda7008a4cc93
JarekSopinski <jarek.sopinski@gmail.com>
2018-06-26 22:17:28 +0200

    Express route params

--- app.js
+++ app.js
@@ -10,4 +10,8 @@ app.get('/contact', function(req, res){
     res.send('This is a contact page')
 })
 
+app.get('/profile/:id', function(req, res){
+    res.send('You requested to see a profile with the id of ' + req.params.id)
+})
+
 app.listen(3000);
