ec91509712cbc11ba17418d8df5952d498093d5b
JarekSopinski <jarek.sopinski@gmail.com>
2018-06-26 14:20:20 +0200

    Basic routing

--- app.js
+++ app.js
@@ -8,18 +8,20 @@ const server = http.createServer(function(req, res) {
 
     console.log('Request was made: ' + req.url);
 
-    res.writeHead(200, {'Content-Type': 'application/json'});
-
-    const myObj = {
-        name: 'Ryu',
-        job: 'Ninja',
-        age: 29
-    };
-
-    res.end(JSON.stringify(myObj));
-
-    // const myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
-    // myReadStream.pipe(res)
+    if(req.url === '/home' || req.url === '/') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/index.html').pipe(res)
+    } else if (req.url === '/contact') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/contact.html').pipe(res)
+    } else if (req.url === '/api/ninjas') {
+        const ninjas = [{name: 'Ryu', age: '29'}, {name: 'Yoshi', age: 32}];
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(ninjas))
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/404.html').pipe(res)
+    }
     
 });
 
