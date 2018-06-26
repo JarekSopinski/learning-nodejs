6c66102eacd1c68eb2133044625fd96628045768
JarekSopinski <jarek.sopinski@gmail.com>
2018-06-26 16:00:13 +0200

    Intro to Express

--- app.js
+++ app.js
@@ -1,29 +1,13 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
 
-const port = 3000;
-const hostname = '192.168.0.24';
+const app = express();
 
-const server = http.createServer(function(req, res) {
+app.get('/', function(req, res){
+    res.send('This is a homepage')
+})
 
-    console.log('Request was made: ' + req.url);
+app.get('/contact', function(req, res){
+    res.send('This is a contact page')
+})
 
-    if(req.url === '/home' || req.url === '/') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        fs.createReadStream(__dirname + '/index.html').pipe(res)
-    } else if (req.url === '/contact') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        fs.createReadStream(__dirname + '/contact.html').pipe(res)
-    } else if (req.url === '/api/ninjas') {
-        const ninjas = [{name: 'Ryu', age: '29'}, {name: 'Yoshi', age: 32}];
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify(ninjas))
-    } else {
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        fs.createReadStream(__dirname + '/404.html').pipe(res)
-    }
-    
-});
-
-server.listen(port, hostname);
-console.log('Yo dawgs, now listening to port 3000')
+app.listen(3000);
