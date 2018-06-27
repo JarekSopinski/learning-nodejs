72c128fecc544056e2923c396d00fb221adbb0b5
JarekSopinski <jarek.sopinski@gmail.com>
2018-06-27 12:13:39 +0200

    Handling POST request

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'));
@@ -11,6 +14,11 @@ app.get('/', function(req, res){
 
 app.get('/contact', function(req, res){
     res.render('contact', {qs: req.query})
+})
+
+app.post('/contact', urlencodedParser, function(req, res){
+    console.log(req.body);
+    res.render('contact-success', {data: req.body})
 })
 
 app.get('/profile/:name', function(req, res){
