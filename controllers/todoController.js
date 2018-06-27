762c4cf6d57d306ea1326e9319d5317f4c96a80e
JarekSopinski <jarek.sopinski@gmail.com>
2018-06-27 16:21:20 +0200

    GET, POST, DELETE conn. to mongo db

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -12,31 +12,42 @@ const todoSchema = new mongoose.Schema({
 });
 
 const Todo = mongoose.model('Todo', todoSchema);
-const itemOne = Todo({item: 'get flowers'}).save(err => {
-    if (err) throw err;
-    console.log('item saved')
-})
 
-let data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
+// const itemOne = Todo({item: 'get flowers'}).save(err => {
+//     if (err) throw err;
+//     console.log('item saved')
+// })
+
+//let data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
 
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 module.exports = function(app){
 
     app.get('/todo', (req, res) => {
-        res.render('todo', {todos: data});
-    });
+        // get data from mongo db and pass it to the view
+        Todo.find({}, (err, data) => {
+            if (err) throw err;
+            res.render('todo', {todos: data});
+        });
+    }); // find () retrieves all items in a collection; Todo.find({item: 'name'}) would return specific item
 
     app.post('/todo', urlencodedParser, (req, res) => {
-        data.push(req.body);
-        res.json(data);
+        // get data from the view and add it to mongo db
+        const newTodo = Todo(req.body).save((err, data) => {
+            if (err) throw err;
+            res.json(data)
+        })
     });
 
     app.delete('/todo/:item', (req, res) => {
-        data = data.filter(todo => {
-            return todo.item.replace(/ /g, '-') !== req.params.item
-        });
-        res.json(data)
+        // delete the requested item from mongo db
+        Todo.find({item: req.params.item
+            .replace(/\-/g, " ")})
+            .remove((err, data) => {
+                if (err) throw err;
+                res.json(data)
+            })
     });
 
 }
